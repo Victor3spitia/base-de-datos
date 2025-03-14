a4c1bfc7c7501962a1add800bbe3d7e0f505cde9
Aprendiz <Aprendiz@BOGDFPCGMF4133.FormacionSena.RED>
2025-03-13 21:15:26 -0500

    otra cosa

--- services/programmingLanguages.js
+++ services/programmingLanguages.js
@@ -6,8 +6,8 @@ async function getMultiple(page = 1) {
   const offset = helper.getOffset(page, config.listPerPage);
   const rows = await db.query(
     `SELECT id, name, released_year, githut_rank, pypl_rank, tiobe_rank 
-    FROM programming_languages LIMIT ${offset},${config.listPerPage}`
-  );
+    FROM programming_languages` /* LIMIT ${offset},${config.listPerPage} */
+  );/* este limit es para que en el get no muestre mas de dies variables */
   const data = helper.emptyOrRows(rows);
   const meta = { page };
 
